37a65e2714c433726967094f9ef371d26ba120c4
11qqqq11 <trantai1531@gmail.com>
2022-10-28 23:15:05 +0700

    Create an edit page

--- src/views/ContactEdit.vue
+++ src/views/ContactEdit.vue
@@ -1,53 +1,38 @@
 <template>
   <div v-if="contact" class="page">
-    <h4 v-if="isAdd">Thêm Liên hệ</h4>
-    <h4 v-else>Hiệu chỉnh Liên hệ</h4>
-    <ContactForm
-      :contact="contact"
-      @submit:contact="onUpdateContact"
-      @delete:contact="onDeleteContact"
-    />
+    <h4>Hiệu chỉnh Liên hệ</h4>
+    <ContactForm :contact="contact" @submit:contact="onUpdateContact" @delete:contact="onDeleteContact" />
     <p>{{ message }}</p>
   </div>
+
 </template>
 <script>
-import ContactForm from "@/components/ContactForm.vue";
-import { contactService } from "@/services/contact.service";
+import ContactForm from '@/components/ContactForm.vue';
+import { contactService } from '@/services/contact.service';
 export default {
   components: {
     ContactForm,
   },
   props: {
-    contactId: { type: Number, required: false },
-    isAdd: { type: Boolean, required: false },
+    contactId: { type: Number, required: true },
   },
   data() {
     return {
       contact: null,
-      message: "",
+      message: '',
     };
   },
   methods: {
     async getContact(id) {
       try {
-        if (id) {
-          this.contact = await contactService.get(id);
-        } else {
-          this.contact = {
-            name: "",
-            email: "",
-            address: "",
-            phone: "",
-            favorite: false,
-          };
-        }
+        this.contact = await contactService.get(id);
       } catch (error) {
         console.log(error);
         // Redirect to NotFound page and keep URL intact
         this.$router.push({
-          name: "notfound",
+          name: 'notfound',
           params: {
-            pathMatch: this.$route.path.split("/").slice(1),
+            pathMatch: this.$route.path.split('/').slice(1)
           },
           query: this.$route.query,
           hash: this.$route.hash,
@@ -56,22 +41,17 @@ export default {
     },
     async onUpdateContact(contact) {
       try {
-        if (this.isAdd) {
-          await contactService.create(contact);
-          this.message = "Thêm Liên hệ thành công";
-        } else {
-          await contactService.update(contact);
-          this.message = "Liên hệ được cập nhật thành công.";
-        }
+        await contactService.update(contact.id, contact);
+        this.message = 'Liên hệ được cập nhật thành công.';
       } catch (error) {
         console.log(error);
       }
     },
     async onDeleteContact(id) {
-      if (confirm("Bạn muốn xóa Liên hệ này?")) {
+      if (confirm('Bạn muốn xóa Liên hệ này?')) {
         try {
           await contactService.delete(id);
-          this.$router.push({ name: "contactbook" });
+          this.$router.push({ name: 'contactbook' });
         } catch (error) {
           console.log(error);
         }
@@ -80,7 +60,7 @@ export default {
   },
   created() {
     this.getContact(this.contactId);
-    this.message = "";
+    this.message = '';
   },
 };
 </script>
